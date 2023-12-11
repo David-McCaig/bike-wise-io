a907c8049ca03f192827f4dbdd8c5e7a89c774ee
David-McCaig <davidmccaig1@gmail.com>
2023-12-11 17:21:54 +1300

    Create chat-submit client component for chat page. Connect to Open Ai API

---
+++ app/chat/chat-submit.tsx
@@ -0,0 +1,30 @@
+import React, { ChangeEvent, FormEvent } from 'react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface ChatSubmitProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+function ChatSubmit({value, onChange, onSubmit}:ChatSubmitProps) {
+  return (
+    <form
+    className="w-2/4 h-40 bg-[#FAFAF9] flex justify-end items-start pb-12 sticky bottom-0"
+    onSubmit={onSubmit}
+  >
+    <div className="flex w-full items-center">
+      <Input
+        className="h-12"
+        value={value}
+        placeholder="Say something..."
+        onChange={onChange}
+      />
+      <Button className="ml-4 w-36 h-[2.8rem]">Send Message</Button>
+    </div>
+  </form>
+  )
+}
+
+export default ChatSubmit
